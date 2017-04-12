db93ad501d487282f3f1b564f3f5d5c68d3a4a8e
luiseduardo3004 <luiseduardo3004@gmail.com>
2017-04-12 03:10:44 -0500

    modificacion2

--- Clima/ControlCiudad.swift
+++ Clima/ControlCiudad.swift
@@ -14,7 +14,7 @@ class ControlCiudad: UIViewController {
     var codigo = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urls = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        let urls = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"
         let url = NSURL(string: urls + codigo + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
         let datos = NSData(contentsOfURL: url!)
         do {
