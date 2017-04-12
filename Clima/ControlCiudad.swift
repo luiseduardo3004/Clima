d2529220c0a998c0511a4a1f30f02eb8db6e7ea7
luiseduardo3004 <luiseduardo3004@gmail.com>
2017-04-12 03:09:06 -0500

    modificacion1

---
+++ Clima/ControlCiudad.swift
@@ -0,0 +1,53 @@
+//
+//  ControlCiudad.swift
+//  Clima
+//
+//  Created by Luis Santamaría on 4/12/17.
+//  Copyright © 2017 Santamaria Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ControlCiudad: UIViewController {
+    @IBOutlet weak var temperatura: UILabel!
+
+    var codigo = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let urls = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        let url = NSURL(string: urls + codigo + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+        let datos = NSData(contentsOfURL: url!)
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+            let dic = json as! NSDictionary
+            let dic1 = dic["query"] as! NSDictionary
+            let dic2 = dic1["results"] as! NSDictionary
+            let dic3 = dic2["channel"] as! NSDictionary
+            let dic4 = dic3["item"] as! NSDictionary
+            let dic5 = dic4["condition"] as! NSDictionary
+            let temp = dic5["temp"] as! NSString
+            self.temperatura.text = temp as String
+        }
+        catch _ {
+            
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Clima/TVC.swift
@@ -0,0 +1,102 @@
+//
+//  TVC.swift
+//  Clima
+//
+//  Created by Luis Santamaría on 4/12/17.
+//  Copyright © 2017 Santamaria Technologies. All rights reserved.
+//
+
+import UIKit
+
+class TVC: UITableViewController {
+
+    private var ciudades: Array<Array<String>> = Array<Array<String>>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Clima de Ciudades"
+        self.ciudades.append(["Dallas, Texas", "dallas%2C%20tx"])
+        self.ciudades.append(["Nome, Alaska", "nome%2C%20ak"])
+        self.ciudades.append(["Maui, Hawaii", "maui%2C%20hi"])
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.ciudades.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Celda", forIndexPath: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = self.ciudades[indexPath.row][0]
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let cc = segue.destinationViewController as! ControlCiudad
+        let ip = self.tableView.indexPathForSelectedRow
+        cc.codigo = self.ciudades[ip!.row][1]
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
